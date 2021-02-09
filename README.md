7b53c3982184a12df8cab84e4d22899aab405e87
David Alberto Díaz Torres <davidadtorres@gmail.com>
2021-02-09 21:58:35 +0000

    0.0.0-61: First Commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # robins10-homepage-pwa
-### `0.0.0-60`
+### `0.0.0-61`
 #### <br/>
 ## Description
 Robin's 10 webpage.
---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Robins10Blog',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+    },
+  ],
+}
